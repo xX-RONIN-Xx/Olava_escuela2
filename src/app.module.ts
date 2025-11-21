37752ad5545f612f4ba5aca1c31f823d5f1ccb7b
Mauricio Dev <microsistemasprogramados@gmail.com>
2025-11-21 19:28:30 -0300

    configuraciones en main y appModule para render

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,42 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CiudadModule } from './ciudad/ciudad.module';
 
 @Module({
+   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT ?? '3306'),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DB,
+      entities: [__dirname + '/**/*.entity.{js,ts}'],
+      synchronize: false, // La DB ya existe en Clever Cloud
+    }),
+    CiudadModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+
+
+
+
+
+  /***************************
   imports: [    TypeOrmModule.forRoot( {
     "type": "mysql",
-    "host": "localhost",
+    "host": "ba4e4i9as5mtw7y63ooi-mysql.services.clever-cloud.com",
     "port": 3306,
-    "username": "root",
-    "password": "admin",
-    "database": "escolar",
-    "entities": [
-          "dist/**/**.entity{.ts,.js}"
-    ],
-    "synchronize": false
+    "username": "uh0ou6fetygfaw5v",
+    "password": "mCbLRMWuqEeJZOAh6Qlt",
+    "database": "ba4e4i9as5mtw7y63ooi",*/
+    //"entities": [__dirname + '/**/*.entity.{js,ts}'], //render compila a dist
+
+          //"dist/**/**.entity{.ts,.js}"],
+   /* "synchronize": false
    }
     ), CiudadModule,
 ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService],*/
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+   app.enableCors({
+    //origin: 'http://localhost:5173', // o '*' en desarrollo
+   origin: [
+  'https://react-escuela.firebaseapp.com',
+  'http://localhost:5173'
+],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
+  //await app.listen(process.env.PORT || 3000);
+
 }
 bootstrap();
